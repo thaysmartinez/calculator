9c8afb25e7826942eab1bd70d6842b0e655a7e5a
Thays Martinez <thaysmartinez@gmail.com>
2023-05-21 17:39:45 +0200

    Add percent function

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const percent = operand => operand / 100;
 
 const operate = (operand1, operator, operand2) => {
   operand1 = Number(operand1);
