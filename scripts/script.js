af6b53a4b21ea60d8ec601cf1c098cd5a74d1bbb
Thays Martinez <thaysmartinez@gmail.com>
2023-05-26 17:31:56 +0200

    Refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
 // Create reference to elements
 const display = document.querySelector('.display-operation');
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator');
-const decimal = document.querySelector('#decimal');
+const digitButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const decimalButton = document.querySelector('#decimal');
 const equalButton = document.querySelector('#equal');
-const allClear = document.querySelector('#ac');
+const allClearButton = document.querySelector('#ac');
+const delButton = document.querySelector('#del');
+const percentButton = document.querySelector('#percent');
 
 // Define operations
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => (b === 0 ? 'error: division by 0' : a / b);
+const divide = (a, b) => (b === 0 ? 'Error' : a / b);
 const percent = operand => operand / 100;
-const ROUND = 10000;
+const ROUND = 10000000000;
 
 const operate = (operand1, operator, operand2) => {
   if (operator === '+') return add(operand1, operand2);
@@ -22,102 +24,93 @@ const operate = (operand1, operator, operand2) => {
 };
 
 const clearMemory = () => {
-  digits = '';
+  digitString = '';
+  operand1;
+  operator;
+  operand2;
   operands = [];
-  operand1 = null;
-  operand2 = null;
-  operator = [];
-  operatorSign = '';
-  result = [];
+  operators = [];
+  results = [];
+  clickEvent = 0;
 };
 
-const clearAll = () => {
-  clearMemory();
-  display.textContent = 0;
-};
-
-// Create variables and arrays to hold operands and operators
-let digits = '';
+let digitString = '';
+let operand1;
+let operand2;
 let operands = [];
-let operand1 = null;
-let operand2 = null;
-let operator = [];
-let operatorSign = '';
-let result = [];
+let operators = [];
+let results = [];
+let operand;
+let operator;
+let clickEvent = 0;
 
-// Populate display
-display.textContent = 0;
-const populateDisplay = () => {
-  numbers.forEach(number =>
-    number.addEventListener('click', () => {
-      display.textContent = digits;
-    })
-  );
-};
+// Update display
 
-allClear.addEventListener('click', clearAll);
-
-// Hold user input
-numbers.forEach(number =>
-  number.addEventListener('click', () => {
-    digits += number.innerText;
-    digits.includes('.')
-      ? (decimal.disabled = true)
-      : (decimal.disabled = false);
-    if (digits.startsWith('.')) {
-      digits = '0' + digits;
-    }
-  })
-);
-
-decimal.addEventListener('click', () => console.log('decimal clicked'));
-
-// Save operators into array
-operators.forEach(operatorBtn =>
-  operatorBtn.addEventListener('click', () => {
-    operator.push(operatorBtn.innerText);
-  })
-);
-
-// Save operands into array
-operators.forEach(operatorBtn =>
-  operatorBtn.addEventListener('click', () => {
-    decimal.disabled = false;
-    operands.unshift(Number(digits));
-    digits = '';
-  })
-);
-
-// Save first user input into variable
-operators.forEach(operatorBtn =>
-  operatorBtn.addEventListener('click', () => {
-    if (result.length === 0) {
-      operand1 = operands[0];
-      result.unshift(operands[0]);
-      display.textContent = operand1;
-    } else {
-      operand2 = operands[0];
-      operatorSign = operator[operator.length - 2];
-      result.unshift(operate(result[0], operatorSign, operand2));
-      operand1 = result[0];
-      display.textContent = operand1;
-    }
-  })
-);
-
-// Call operate function when user clicks on the equal sign
-equalButton.addEventListener('click', () => {
-  operands.unshift(Number(digits));
-  operatorSign = operator[operator.length - 1];
-  operand2 = operands[0];
-  let total = operate(operand1, operatorSign, operand2);
-  total = Math.round(total * ROUND) / ROUND;
-  display.textContent = total;
-  //   clearMemory();
+delButton.addEventListener('click', () => {
+  digitString = display.innerText.slice(0, -1);
+  display.textContent = digitString;
 });
 
-populateDisplay();
+allClearButton.addEventListener('click', () => {
+  display.textContent = 0;
+  clearMemory();
+});
 
-// Fix issue when user only clicks on operator buttons
-// display 0 before decimal if user starts operation with decimaml
-// Implement percent operation
+percentButton.addEventListener('click', () => {
+  digitString /= 100;
+  display.textContent = digitString;
+});
+
+decimalButton.addEventListener('click', () => {
+  decimalButton.disabled = true;
+});
+
+display.textContent = '0';
+digitButtons.forEach(digitButton => {
+  digitButton.addEventListener('click', () => {
+    digitString += digitButton.innerText;
+    digitString.startsWith('.')
+      ? (display.textContent = digitString = '0.')
+      : (display.textContent = digitString);
+  });
+});
+
+operatorButtons.forEach(operatorButton => {
+  operatorButton.addEventListener('click', () => {
+    decimalButton.disabled = false;
+    operands.push(Number(digitString));
+    operators.unshift(operatorButton.innerText);
+    digitString = '';
+
+    if (clickEvent === 0) {
+      operand1 = operands[0];
+      operator = operators[0];
+      operand2 = 0;
+      results.unshift(Math.round(operand1 * ROUND) / ROUND);
+    } else {
+      operand1 = results[0];
+      operator = operators[1];
+      operand2 = operands[operands.length - 1];
+      results.unshift(
+        Math.round(operate(operand1, operator, operand2) * ROUND) / ROUND
+      );
+    }
+    display.textContent = results[0];
+    clickEvent++;
+
+    console.log('operand1:', operand1);
+    console.log('operand2:', operand2);
+    console.log('operator:', operator);
+    console.log('results:', results);
+  });
+});
+
+equalButton.addEventListener('click', () => {
+  delButton.disabled = true;
+  operand1 = results[0];
+  operator = operators[0];
+  operand2 = Number(digitString);
+  results.unshift(operate(operand1, operator, operand2));
+  display.textContent = Math.round(results[0] * ROUND) / ROUND;
+  clearMemory();
+});
