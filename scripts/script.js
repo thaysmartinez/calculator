8522441429fca8a931670115ab361a53e3d29c05
Thays Martinez <thaysmartinez@gmail.com>
2023-06-02 14:38:48 +0200

    Organize code

--- scripts/script.js
+++ scripts/script.js
@@ -23,28 +23,14 @@ const operate = (operand1, operator, operand2) => {
   if (operator === '÷' || operator === '/') return divide(operand1, operand2);
 };
 
-// Define support functions
-
-const truncateNumber = value => {
-  // check if the value has a decimal point
-  const hasDecimal = value.includes('.');
-
-  // Check if value is longer than 10 characters (including the decimal point)
-  // Truncate the value to 10 characters
-  if (value.length > 11 && !hasDecimal) value = value.slice(0, 11);
-
-  // Truncate the value to 11 characters (including the decimal point)
-  if (value.length > 12 && hasDecimal) value = value.slice(0, 12);
-
-  display.textContent = value;
-};
-
+// Define variables
 let digitString = '';
 let operands = [];
 let operators = [];
 let decimalEntered = false;
 display.textContent = '0';
 
+// Add event listeners
 document.addEventListener('keydown', handleKeyDown);
 allClearButton.addEventListener('click', handleAllClearInput);
 delButton.addEventListener('click', handleDeleteInput);
@@ -60,6 +46,21 @@ operatorButtons.forEach(operatorButton => {
     handleOperatorInput(operatorButton.innerText)
   );
 });
+
+// Define support functions
+const truncateNumber = value => {
+  // check if the value has a decimal point
+  const hasDecimal = value.includes('.');
+
+  // Check if value is longer than 10 characters (including the decimal point)
+  // Truncate the value to 10 characters
+  if (value.length > 11 && !hasDecimal) value = value.slice(0, 11);
+
+  // Truncate the value to 11 characters (including the decimal point)
+  if (value.length > 12 && hasDecimal) value = value.slice(0, 12);
+
+  display.textContent = value;
+};
 
 function handleKeyDown(event) {
   const key = event.key;
@@ -118,7 +119,6 @@ function handleDecimalInput() {
       digitString = display.textContent;
     }
 
-    // display.textContent = digitString;
     decimalEntered = true;
     decimalButton.disabled = true;
   }
@@ -143,13 +143,13 @@ function handleEqualsInput() {
   // store last number input into calculator display
   operands.push(Number(display.innerText));
 
-  // display result
+  // calculate
   let result = operate(
     operands[operands.length - 2],
     operators[operators.length - 1],
     operands[operands.length - 1]
   );
-  //   display.textContent = result;
+
   truncateNumber(result.toString());
 
   // store result
